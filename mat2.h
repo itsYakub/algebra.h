4b1ed4d98f437d1601efc8ef3b2642eb051dcca6
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-02-23 09:29:26 +0100

    algebra: c: updated matrices

--- mat2.h
+++ mat2.h
@@ -14,19 +14,17 @@ union u_mat2 {
 
 extern mat2 mat2Zero(void);
 
-extern mat2 mat2Identity(void);
+extern mat2 mat2Init(float);
 
-extern mat2 mat2Scalar(const float);
+extern mat2 mat2Add(mat2, mat2);
 
-extern mat2 mat2Add(const mat2, const mat2);
+extern mat2 mat2Sub(mat2, mat2);
 
-extern mat2 mat2Sub(const mat2, const mat2);
+extern mat2 mat2Mul(mat2, mat2);
 
-extern mat2 mat2Mul(const mat2, const mat2);
+extern mat2 mat2Mulf(mat2, float);
 
-extern mat2 mat2Mulf(const mat2, const float);
-
-extern float mat2Det(const mat2);
+extern float mat2Det(mat2);
 
 # if defined (ALGEBRA_IMPLEMENTATION)
 
@@ -35,39 +33,34 @@ extern mat2 mat2Zero(void) {
                       0.0, 0.0 }} );
 }
 
-extern mat2 mat2Identity(void) {
-    return ((mat2) {{ 1.0, 0.0,
-                      0.0, 1.0 }} );
-}
-
-extern mat2 mat2Scalar(const float s) {
+extern mat2 mat2Init(float s) {
     return ((mat2) {{ 1.0 * s, 0.0,
                       0.0,     1.0 * s }} );
 }
 
-extern mat2 mat2Add(const mat2 a, const mat2 b) {
+extern mat2 mat2Add(mat2 a, mat2 b) {
     return ((mat2) {{ a.m00 + b.m00, a.m01 + b.m01,
                       a.m10 + b.m10, a.m11 + b.m11 }} );
 }
 
-extern mat2 mat2Sub(const mat2 a, const mat2 b) {
+extern mat2 mat2Sub(mat2 a, mat2 b) {
     return ((mat2) {{ a.m00 - b.m00, a.m01 - b.m01,
                       a.m10 - b.m10, a.m11 - b.m11 }} );
 }
 
-extern mat2 mat2Mul(const mat2 a, const mat2 b) {
+extern mat2 mat2Mul(mat2 a, mat2 b) {
     return ((mat2) {{ a.m00 * b.m00 + a.m10 * b.m01,
                       a.m01 * b.m00 + a.m11 * b.m01,
                       a.m00 * b.m10 + a.m10 * b.m11,
                       a.m01 * b.m10 + a.m11 * b.m11 }} );
 }
 
-extern mat2 mat2Mulf(const mat2 a, const float f) {
+extern mat2 mat2Mulf(mat2 a, float f) {
     return ((mat2) {{ a.m00 * f, a.m01 * f,
                       a.m10 * f, a.m11 * f }} );
 }
 
-extern float mat2Det(const mat2 a) {
+extern float mat2Det(mat2 a) {
     return (a.m00 * a.m11 - a.m10 * a.m01);
 }
 
--- mat3.h
+++ mat3.h
@@ -15,17 +15,17 @@ union u_mat3 {
 
 extern mat3 mat3Zero(void);
 
-extern mat3 mat3Init(const float);
+extern mat3 mat3Init(float);
 
-extern mat3 mat3Add(const mat3, const mat3);
+extern mat3 mat3Add(mat3, mat3);
 
-extern mat3 mat3Sub(const mat3, const mat3);
+extern mat3 mat3Sub(mat3, mat3);
 
-extern mat3 mat3Mul(const mat3, const mat3);
+extern mat3 mat3Mul(mat3, mat3);
 
-extern mat3 mat3Mulf(const mat3, const float);
+extern mat3 mat3Mulf(mat3, float);
 
-extern float mat3Det(const mat3);
+extern float mat3Det(mat3);
 
 # if defined (ALGEBRA_IMPLEMENTATION)
 
@@ -35,25 +35,25 @@ extern mat3 mat3Zero(void) {
                       0.0, 0.0, 0.0 }} );
 }
 
-extern mat3 mat3Init(const float s) {
+extern mat3 mat3Init(float s) {
     return ((mat3) {{ 1.0 * s, 0.0,     0.0,
                       0.0,     1.0 * s, 0.0,
                       0.0,     0.0,     1.0 * s }} );
 }
 
-extern mat3 mat3Add(const mat3 a, const mat3 b) {
+extern mat3 mat3Add(mat3 a, mat3 b) {
     return ((mat3) {{ a.m00 + b.m00, a.m01 + b.m01, a.m02 + b.m02,
                       a.m10 + b.m10, a.m11 + b.m11, a.m12 + b.m12,
                       a.m20 + b.m20, a.m21 + b.m21, a.m22 + b.m22 }} );
 }
 
-extern mat3 mat3Sub(const mat3 a, const mat3 b) {
+extern mat3 mat3Sub(mat3 a, mat3 b) {
     return ((mat3) {{ a.m00 - b.m00, a.m01 - b.m01, a.m02 - b.m02,
                       a.m10 - b.m10, a.m11 - b.m11, a.m12 - b.m12,
                       a.m20 - b.m20, a.m21 - b.m21, a.m22 - b.m22 }} );
 }
 
-extern mat3 mat3Mul(const mat3 a, const mat3 b) {
+extern mat3 mat3Mul(mat3 a, mat3 b) {
     return ((mat3) {{ a.m00 * b.m00 + a.m10 * b.m01 + a.m20 * b.m02,
                       a.m01 * b.m00 + a.m11 * b.m01 + a.m21 * b.m02,
                       a.m02 * b.m00 + a.m12 * b.m01 + a.m22 * b.m02,
@@ -67,13 +67,13 @@ extern mat3 mat3Mul(const mat3 a, const mat3 b) {
                       a.m02 * b.m20 + a.m12 * b.m21 + a.m22 * b.m22 }} );
 }
 
-extern mat3 mat3Mulf(const mat3 a, const float f) {
+extern mat3 mat3Mulf(mat3 a, float f) {
     return ((mat3) {{ a.m00 * f, a.m01 * f, a.m02 * f,
                       a.m10 * f, a.m11 * f, a.m12 * f,
                       a.m20 * f, a.m21 * f, a.m22 * f }} );
 }
 
-extern float mat3Det(const mat3 a) {
+extern float mat3Det(mat3 a) {
     return (a.m00 * a.m11 * a.m22 +
             a.m01 * a.m12 * a.m20 +
             a.m02 * a.m10 * a.m21 -
