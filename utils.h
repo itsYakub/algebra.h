e60b5903fda3ed1b8da97eefab3af14a9a6c564e
Jakub Oleksiak <joleksia@c3r11s1.42warsaw.pl>
2026-02-25 07:43:26 +0100

    algebra: utils: initial commit

---
+++ utils.h
@@ -0,0 +1,38 @@
+#if !defined (_utils_h_)
+# define _utils_h_ 1
+#
+# if !defined (PI)
+#  define PI (3.14159)
+# endif /* PI */
+#
+# if !defined (E)
+#  define E 2.71828
+# endif /* E */
+#
+# if !defined (EPSILON)
+#  define EPSILON (1e-6)
+# endif /* EPSILON */
+#
+# if !defined (NaN)
+#  define NaN (0.0 / 0.0)
+# endif /* EPSILON */
+#
+# if !defined (INFINITY)
+#  define INFINITY (1e100)
+# endif /* EPSILON */
+
+/* NOTE:
+ *  Static-inlining those functions because the cost of binary-size is cheaper then cost of context switch.
+ *  Like, cmon, these functions could be macro - functions as well...
+ * */
+
+static inline float deg2rad(float);
+
+static inline float rad2deg(float);
+
+
+static inline float deg2rad(float f) { return (f * (PI / 180.0); }
+
+static inline float rad2deg(float f) { return (f * (180.0 / PI); }
+
+#endif /* _utils_h_ */
---
+++ utils.hpp
@@ -0,0 +1,17 @@
+#if !defined (_utils_hpp_)
+# define _utils_hpp_ 1
+#
+# if defined (__cplusplus)
+
+extern "C" {
+
+# endif
+#
+# include "./utils.h"
+#
+# if defined (__cplusplus)
+
+}
+
+# endif
+#endif /* _utils_hpp_ */
