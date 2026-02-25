86d3f54a5c185d51cb7ae3ac265999c1b5b34057
Jakub Oleksiak <joleksia@c3r11s1.42warsaw.pl>
2026-02-25 12:01:29 +0100

    algebra: c++: mat4 lookAt

--- mat4.hpp
+++ mat4.hpp
@@ -345,12 +345,12 @@ mat4 mat4::lookAt(vec3 eye, vec3 center, vec3 up) {
     u = s.cross(f);
 
     mat4 mat = mat4(1.0);
-    mat.m00 = s.x;  mat.m10 = s.y;  mat.m20 = s.z;
-    mat.m01 = u.x;  mat.m11 = u.y;  mat.m21 = u.z;
-    mat.m02 = f.x;  mat.m12 = f.y;  mat.m22 = f.z;
-    mat.m03 = -s.dot(eye);
-    mat.m13 = -u.dot(eye);
-    mat.m23 = -f.dot(eye);
+    mat.m00 =  s.x; mat.m10 =  s.y; mat.m20 =  s.z;
+    mat.m01 =  u.x; mat.m11 =  u.y; mat.m21 =  u.z;
+    mat.m02 = -f.x; mat.m12 = -f.y; mat.m22 = -f.z;
+    mat.m30 = -s.dot(eye);
+    mat.m31 = -u.dot(eye);
+    mat.m12 = -f.dot(eye);
     return (mat);
 }
 
