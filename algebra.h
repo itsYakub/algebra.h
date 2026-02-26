04e01f8eeb1eb7ebc63fa321f500ddf34ed7c12f
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-02-26 14:17:03 +0100

    algebra: language fallback
    algebra.h and algebra.hpp falls back to the correct lanaguage if included in wrong program; if algebra.h'd be included in cpp file, it would fall back to algebra.hpp and vice versa.

--- algebra.h
+++ algebra.h
@@ -1,15 +1,20 @@
 #if defined (__cplusplus)
 # pragma once
 #endif /* __cplusplus */
+#
 #if !defined (_algebra_h_)
 # define _algebra_h_ 1
 #
-# include "vec2.h"
-# include "vec3.h"
-# include "vec4.h"
-# include "mat2.h"
-# include "mat3.h"
-# include "mat4.h"
-# include "utils.h"
-#
-# endif /* _algebra_h_ */
+# /* fall-back to C++ */
+# if defined (__cplusplus)
+#  include "algebra.hpp"
+# else
+#  include "vec2.h"
+#  include "vec3.h"
+#  include "vec4.h"
+#  include "mat2.h"
+#  include "mat3.h"
+#  include "mat4.h"
+#  include "utils.h"
+# endif /* __cplusplus */
+#endif /* _algebra_h_ */
--- algebra.hpp
+++ algebra.hpp
@@ -1,12 +1,20 @@
+#if defined (__cplusplus)
+# pragma once
+#endif /* __cplusplus */
+#
 #if !defined (_algebra_hpp_)
 # define _algebra_hpp_ 1
 #
-# include "vec2.hpp"
-# include "vec3.hpp"
-# include "vec4.hpp"
-# include "mat2.hpp"
-# include "mat3.hpp"
-# include "mat4.hpp"
-# include "utils.hpp"
-#
-# endif /* _algebra_hpp_ */
+# /* fall-back to C */
+# if !defined (__cplusplus)
+#  include "algebra.h"
+# else
+#  include "vec2.hpp"
+#  include "vec3.hpp"
+#  include "vec4.hpp"
+#  include "mat2.hpp"
+#  include "mat3.hpp"
+#  include "mat4.hpp"
+#  include "utils.hpp"
+# endif /* __cplusplus */
+#endif /* _algebra_hpp_ */
