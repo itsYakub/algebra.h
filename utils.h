914fb8908c1d126d0c75a049d08fd36ded02ace5
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-03-04 21:55:03 +0100

    algebra: utils: functions to macro-functions

--- utils.h
+++ utils.h
@@ -20,19 +20,37 @@
 # if !defined (INFINITY)
 #  define INFINITY (1e100)
 # endif /* EPSILON */
-
-static inline float deg2rad(float f) { return (f * (PI / 180.0)); }
-
-static inline float rad2deg(float f) { return (f * (180.0 / PI)); }
-
-static inline float min(float a, float b) { return (a < b ? a : b); }
-
-static inline float max(float a, float b) { return (a > b ? a : b); }
-
-static inline float clamp(float f, float a, float b) { return (min(max(f, a), b)); }
-
-static inline float clamp01(float f) { return (min(max(f, 0), 1)); }
-
-static inline float lerp(float a, float b, float t) { return ((b - a) * t + a); }
-
+#
+# if !defined (deg2rad)
+#  define deg2rad(f) ((f) * ((PI) / 180.0))
+# endif /* deg2rad */
+#
+# if !defined (rad2deg)
+#  define rad2deg(f) ((f) * (180.0 / (PI)))
+# endif /* rad2deg */
+#
+# if !defined (min)
+#  define min(a, b) ((a) < (b) ? (a) : (b))
+# endif /* min */
+#
+# if !defined (max)
+#  define max(a, b) ((a) > (b) ? (a) : (b))
+# endif /* max */
+#
+# if !defined (clamp)
+#  define clamp(f, a, b) (min(max((f), (a)), (b)))
+# endif /* clamp */
+#
+# if !defined (clamp01)
+#  define clamp01(f) (clamp((f), 0.0, 1.0))
+# endif /* clamp01 */
+#
+# if !defined (lerp)
+#  define lerp(a, b, t) (((b) - (a)) * (t) + (a))
+# endif /* lerp */
+#
+# if !defined (lerp01)
+#  define lerp01(a, b, t) (lerp(a, b, clamp01(t)))
+# endif /* lerp01 */
+#
 #endif /* _utils_h_ */
