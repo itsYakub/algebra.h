3754281fec9174b24bb1c8bf65f15a35f35c77c0
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-03-06 11:31:34 +0100

    algebra: vec3: properties for C++
    Added a few built-in properties and boolean operator overloads for vec3.

--- vec3.hpp
+++ vec3.hpp
@@ -29,6 +29,24 @@ struct vec3 {
     vec3(float, float, float);
 
     vec3(const vec3 &);
+
+    /* properties... */
+
+    static const vec3 zero;
+
+    static const vec3 one;
+
+    static const vec3 right;
+
+    static const vec3 left;
+
+    static const vec3 up;
+
+    static const vec3 down;
+
+    static const vec3 front;
+
+    static const vec3 back;
    
     /* operator overloading... */
 
@@ -65,6 +83,18 @@ struct vec3 {
     vec3 &operator *= (float);
 
     vec3 &operator /= (float);
+
+    bool operator == (vec3) const;
+    
+    bool operator != (vec3) const;
+    
+    bool operator > (vec3) const;
+    
+    bool operator >= (vec3) const;
+    
+    bool operator < (vec3) const;
+    
+    bool operator <= (vec3) const;
    
     /* static methods... */
     
@@ -72,20 +102,54 @@ struct vec3 {
     
     static float length(vec3);
 
+    static float distance(vec3, vec3);
+
     static vec3 cross(vec3, vec3);
     
     static vec3 normalize(vec3);
 };
 
 # if defined (ALGEBRA_IMPLEMENTATION)
+ 
+/* constructors... */
 
 vec3::vec3(void) : x(0.0), y(0.0), z(0.0) { }
 
+
 vec3::vec3(float v) : x(v), y(v), z(v) { }
+
 
 vec3::vec3(float x, float y, float z) : x(x), y(y), z(z) { }
 
+
 vec3::vec3(const vec3 &other) : x(other.x), y(other.y), z(other.z) { }
+
+/* properties... */
+
+const vec3 vec3::zero = vec3(0.0);
+
+
+const vec3 vec3::one = vec3(1.0);
+
+
+const vec3 vec3::right = vec3(1.0, 0.0, 0.0);
+
+
+const vec3 vec3::left = vec3(-1.0, 0.0, 0.0);
+
+
+const vec3 vec3::up = vec3(0.0, 1.0, 0.0);
+
+
+const vec3 vec3::down = vec3(0.0, -1.0, 0.0);
+
+
+const vec3 vec3::front = vec3(0.0, 0.0, 1.0);
+
+
+const vec3 vec3::back = vec3(0.0, 0.0, -1.0);
+
+/* operator overloading... */
 
 vec3 &vec3::operator = (const vec3 &other) {
     this->x = other.x;
@@ -94,17 +158,20 @@ vec3 &vec3::operator = (const vec3 &other) {
     return (*this);
 }
 
+
 vec3 vec3::operator + (vec3 other) const {
     return (vec3(this->x + other.x,
                  this->y + other.y,
                  this->z + other.z));
 }
 
+
 vec3 vec3::operator - (vec3 other) const {
     return (vec3(this->x - other.x,
-                 this->y - other.y,
+                this->y - other.y,
                  this->z - other.z));
 }
+
 
 vec3 vec3::operator * (vec3 other) const {
     return (vec3(this->x * other.x,
@@ -112,11 +179,13 @@ vec3 vec3::operator * (vec3 other) const {
                  this->z * other.z));
 }
 
+
 vec3 vec3::operator / (vec3 other) const {
     return (vec3(other.x != 0.0 ? this->x / other.x : 0.0,
                  other.y != 0.0 ? this->y / other.y : 0.0,
                  other.z != 0.0 ? this->z / other.z : 0.0));
 }
+
 
 vec3 vec3::operator + (float f) const {
     return (vec3(this->x + f,
@@ -124,11 +193,13 @@ vec3 vec3::operator + (float f) const {
                  this->z + f));
 }
 
+
 vec3 vec3::operator - (float f) const {
     return (vec3(this->x - f,
                  this->y - f,
                  this->z - f));
 }
+
 
 vec3 vec3::operator * (float f) const {
     return (vec3(this->x * f,
@@ -136,11 +207,13 @@ vec3 vec3::operator * (float f) const {
                  this->z * f));
 }
 
+
 vec3 vec3::operator / (float f) const {
     return (vec3(f != 0.0 ? this->x / f : 0.0,
                  f != 0.0 ? this->y / f : 0.0,
                  f != 0.0 ? this->z / f : 0.0));
 }
+
 
 vec3 &vec3::operator += (vec3 other) {
     this->x += other.x;
@@ -149,12 +222,14 @@ vec3 &vec3::operator += (vec3 other) {
     return (*this);
 }
 
+
 vec3 &vec3::operator -= (vec3 other) {
     this->x -= other.x;
     this->y -= other.y;
     this->z -= other.z;
     return (*this);
 }
+
 
 vec3 &vec3::operator *= (vec3 other) {
     this->x *= other.x;
@@ -163,12 +238,14 @@ vec3 &vec3::operator *= (vec3 other) {
     return (*this);
 }
 
+
 vec3 &vec3::operator /= (vec3 other) {
     this->x = other.x != 0.0 ? this->x / other.x : 0.0;
     this->y = other.y != 0.0 ? this->y / other.y : 0.0;
     this->z = other.z != 0.0 ? this->z / other.z : 0.0;
     return (*this);
 }
+
 
 vec3 &vec3::operator += (float f) {
     this->x += f;
@@ -177,12 +254,14 @@ vec3 &vec3::operator += (float f) {
     return (*this);
 }
 
+
 vec3 &vec3::operator -= (float f) {
     this->x -= f;
     this->y -= f;
     this->z -= f;
     return (*this);
 }
+
 
 vec3 &vec3::operator *= (float f) {
     this->x *= f;
@@ -191,6 +270,7 @@ vec3 &vec3::operator *= (float f) {
     return (*this);
 }
 
+
 vec3 &vec3::operator /= (float f) {
     this->x = f != 0.0 ? this->x / f : 0.0;
     this->y = f != 0.0 ? this->y / f : 0.0;
@@ -198,13 +278,66 @@ vec3 &vec3::operator /= (float f) {
     return (*this);
 }
 
+
+bool vec3::operator == (vec3 other) const {
+    return (this->x == other.x &&
+            this->y == other.y &&
+            this->z == other.z);
+}
+
+
+bool vec3::operator != (vec3 other) const {
+    return (this->x != other.x ||
+            this->y != other.y ||
+            this->z != other.z);
+}
+
+
+bool vec3::operator > (vec3 other) const {
+    return (this->x > other.x ||
+            this->y > other.y ||
+            this->z > other.z);
+}
+
+
+bool vec3::operator >= (vec3 other) const {
+    return (this->x >= other.x ||
+            this->y >= other.y ||
+            this->z >= other.z);
+}
+
+
+bool vec3::operator < (vec3 other) const {
+    return (this->x < other.x ||
+            this->y < other.y ||
+            this->z < other.z);
+}
+
+
+bool vec3::operator <= (vec3 other) const {
+    return (this->x <= other.x ||
+            this->y <= other.y ||
+            this->z <= other.z);
+}
+
+/* static methods... */
+
 float vec3::dot(vec3 a, vec3 b) {
     return (a.x * b.x + a.y * b.y + a.z * b.z);
 }
 
+
 float vec3::length(vec3 a) {
-    return (sqrt(pow(a.x, 2) + pow(a.y, 2) + pow(a.z, 2)));
+    return (sqrt(a.x * a.x + a.y * a.y + a.z * a.z));
 }
+
+
+float vec3::distance(vec3 a, vec3 b) {
+    return (sqrt((a.x - b.x) * (a.x - b.x) +
+                 (a.y - b.y) * (a.y * b.y) +
+                 (a.z - b.z) * (a.z * b.z)));
+}
+
 
 vec3 vec3::cross(vec3 a, vec3 b) {
     vec3 vec;
@@ -214,6 +347,7 @@ vec3 vec3::cross(vec3 a, vec3 b) {
     vec.z = a.x * b.y - a.y * b.x;
     return (vec);
 }
+
 
 vec3 vec3::normalize(vec3 a) {
     float len = vec3::length(a);
