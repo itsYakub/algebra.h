03860392751834e243fd932c3707349251c4b9b1
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-03-06 11:45:03 +0100

    algebra: vec3: add spacing in C implementation

--- vec3.h
+++ vec3.h
@@ -81,33 +81,41 @@ extern const vec3 vec3Zero(void) {
     return ((vec3) { 0.0, 0.0, 0.0 } );
 }
 
+
 extern const vec3 vec3One(void) {
     return ((vec3) { 1.0, 1.0, 1.0 } );
 }
+
 
 extern const vec3 vec3Right(void) {
     return ((vec3) { 1.0, 0.0, 0.0 } );
 }
 
+
 extern const vec3 vec3Left(void) {
     return ((vec3) { -1.0, 0.0, 0.0 } );
 }
+
 
 extern const vec3 vec3Up(void) {
     return ((vec3) { 0.0, 1.0, 0.0 } );
 }
 
+
 extern const vec3 vec3Down(void) {
     return ((vec3) { 0.0, -1.0, 0.0 } );
 }
+
 
 extern const vec3 vec3Front(void) {
     return ((vec3) { 0.0, 0.0, 1.0 } );
 }
 
+
 extern const vec3 vec3Back(void) {
     return ((vec3) { 0.0, 0.0, -1.0 } );
 }
+
 
 extern vec3 vec3Add(vec3 a, vec3 b) {
     return ((vec3) { .x = a.x + b.x,
@@ -115,11 +123,13 @@ extern vec3 vec3Add(vec3 a, vec3 b) {
                      .z = a.z + b.z } );
 }
 
+
 extern vec3 vec3Sub(vec3 a, vec3 b) {
     return ((vec3) { .x = a.x - b.x,
                      .y = a.y - b.y,
                      .z = a.z - b.z } );
 }
+
 
 extern vec3 vec3Mul(vec3 a, vec3 b) {
     return ((vec3) { .x = a.x * b.x,
@@ -127,11 +137,13 @@ extern vec3 vec3Mul(vec3 a, vec3 b) {
                      .z = a.z * b.z } );
 }
 
+
 extern vec3 vec3Div(vec3 a, vec3 b) {
     return ((vec3) { .x = b.x != 0.0 ? a.x / b.x : 0.0,
                      .y = b.y != 0.0 ? a.y / b.y : 0.0,
                      .z = b.z != 0.0 ? a.z / b.z : 0.0 } );
 }
+
 
 extern vec3 vec3Addf(vec3 a, float f) {
     return ((vec3) { .x = a.x + f,
@@ -139,11 +151,13 @@ extern vec3 vec3Addf(vec3 a, float f) {
                      .z = a.z + f } );
 }
 
+
 extern vec3 vec3Subf(vec3 a, float f) {
     return ((vec3) { .x = a.x - f,
                      .y = a.y - f,
                      .z = a.z - f } );
 }
+
 
 extern vec3 vec3Mulf(vec3 a, float f) {
     return ((vec3) { .x = a.x * f,
@@ -151,11 +165,13 @@ extern vec3 vec3Mulf(vec3 a, float f) {
                      .z = a.z * f } );
 }
 
+
 extern vec3 vec3Divf(vec3 a, float f) {
     return ((vec3) { .x = f != 0.0 ? a.x / f : 0.0,
                      .y = f != 0.0 ? a.y / f : 0.0,
                      .z = f != 0.0 ? a.z / f : 0.0 } );
 }
+
 
 extern bool vec3Equal(vec3 a, vec3 b) {
     return (a.x == b.x &&
@@ -163,11 +179,13 @@ extern bool vec3Equal(vec3 a, vec3 b) {
             a.z == b.z);
 }
 
+
 extern bool vec3NoEqual(vec3 a, vec3 b) {
     return (a.x != b.x ||
             a.y != b.y ||
             a.z != b.z);
 }
+
 
 extern bool vec3Great(vec3 a, vec3 b) {
     return (a.x > b.x ||
@@ -175,11 +193,13 @@ extern bool vec3Great(vec3 a, vec3 b) {
             a.z > b.z);
 }
 
+
 extern bool vec3GreatEqual(vec3 a, vec3 b) {
     return (a.x >= b.x ||
             a.y >= b.y ||
             a.z >= b.z);
 }
+
 
 extern bool vec3Less(vec3 a, vec3 b) {
     return (a.x < b.x ||
@@ -187,25 +207,30 @@ extern bool vec3Less(vec3 a, vec3 b) {
             a.z < b.z);
 }
 
+
 extern bool vec3LessEqual(vec3 a, vec3 b) {
     return (a.x <= b.x ||
             a.y <= b.y ||
             a.z <= b.z);
 }
 
+
 extern float vec3Dot(vec3 a, vec3 b) {
     return (a.x * b.x + a.y * b.y + a.z * b.z);
 }
 
+
 extern float vec3Length(vec3 a) {
     return (sqrt(a.x * a.x + a.y * a.y + a.z * a.z));
 }
+
 
 extern float vec3Distance(vec3 a, vec3 b) {
     return (sqrt((a.x - b.x) * (a.x - b.x) +
                  (a.y - b.y) * (a.y * b.y) +
                  (a.z - b.z) * (a.z * b.z)));
 }
+
 
 extern vec3 vec3Cross(vec3 a, vec3 b) {
     vec3 vec;
@@ -215,6 +240,7 @@ extern vec3 vec3Cross(vec3 a, vec3 b) {
     vec.z = a.x * b.y - a.y * b.x;
     return (vec);
 }
+
 
 extern vec3 vec3Normalize(vec3 a) {
     float len = vec3Length(a);
