c8ffb9cf8d5c23a89f8a8dc69d4cccfe1013e800
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-03-08 10:19:06 +0100

    algebra: vec4: updated API

--- vec4.h
+++ vec4.h
@@ -21,6 +21,22 @@ union u_vec4 {
     float ptr[4];
 };
 
+extern const vec4 vec4Zero(void);
+
+extern const vec4 vec4One(void);
+
+extern const vec4 vec4Right(void);
+
+extern const vec4 vec4Left(void);
+
+extern const vec4 vec4Up(void);
+
+extern const vec4 vec4Down(void);
+
+extern const vec4 vec4Front(void);
+
+extern const vec4 vec4Back(void);
+
 extern vec4 vec4Add(vec4, vec4);
 
 extern vec4 vec4Sub(vec4, vec4);
@@ -37,13 +53,74 @@ extern vec4 vec4Mulf(vec4, float);
 
 extern vec4 vec4Divf(vec4, float);
 
+extern bool vec4Equal(vec4, vec4);
+
+extern bool vec4NoEqual(vec4, vec4);
+
+extern bool vec4Great(vec4, vec4);
+
+extern bool vec4GreatEqual(vec4, vec4);
+
+extern bool vec4Less(vec4, vec4);
+
+extern bool vec4LessEqual(vec4, vec4);
+
+extern float vec4Dot(vec4, vec4);
+
+extern float vec4Length(vec4);
+
+extern float vec4Distance(vec4, vec4);
+
+extern vec4 vec4Normalize(vec4);
+
 # if defined (ALGEBRA_IMPLEMENTATION)
+
+extern const vec4 vec4Zero(void) {
+    return ((vec4) { 0.0, 0.0, 0.0, 0.0 } );
+}
+
+
+extern const vec4 vec4One(void) {
+    return ((vec4) { 1.0, 1.0, 1.0, 1.0 } );
+}
+
+
+extern const vec4 vec4Right(void) {
+    return ((vec4) { 1.0, 0.0, 0.0, 0.0 } );
+}
+
+
+extern const vec4 vec4Left(void) {
+    return ((vec4) { -1.0, 0.0, 0.0, 0.0 } );
+}
+
+
+extern const vec4 vec4Up(void) {
+    return ((vec4) { 0.0, 1.0, 0.0, 0.0 } );
+}
+
+
+extern const vec4 vec4Down(void) {
+    return ((vec4) { 0.0, -1.0, 0.0, 0.0 } );
+}
+
+
+extern const vec4 vec4Front(void) {
+    return ((vec4) { 0.0, 0.0, 1.0, 0.0 } );
+}
+
+
+extern const vec4 vec4Back(void) {
+    return ((vec4) { 0.0, 0.0, -1.0, 0.0 } );
+}
+
 
 extern vec4 vec4Add(vec4 a, vec4 b) {
     return ((vec4) { .x = a.x + b.x,
                      .z = a.z + b.z,
                      .w = a.w + b.w } );
 }
+
 
 extern vec4 vec4Sub(vec4 a, vec4 b) {
     return ((vec4) { .x = a.x - b.x,
@@ -52,12 +129,14 @@ extern vec4 vec4Sub(vec4 a, vec4 b) {
                      .w = a.w - b.w } );
 }
 
+
 extern vec4 vec4Mul(vec4 a, vec4 b) {
     return ((vec4) { .x = a.x * b.x,
                      .y = a.y * b.y,
                      .z = a.z * b.z,
                      .w = a.w * b.w } );
 }
+
 
 extern vec4 vec4Div(vec4 a, vec4 b) {
     return ((vec4) { .x = b.x != 0.0 ? a.x / b.x : 0.0,
@@ -66,12 +145,14 @@ extern vec4 vec4Div(vec4 a, vec4 b) {
                      .w = b.w != 0.0 ? a.w / b.w : 0.0 } );
 }
 
+
 extern vec4 vec4Addf(vec4 a, float f) {
     return ((vec4) { .x = a.x + f,
                      .y = a.y + f,
                      .z = a.z + f,
                      .w = a.w + f } );
 }
+
 
 extern vec4 vec4Subf(vec4 a, float f) {
     return ((vec4) { .x = a.x + f,
@@ -80,6 +161,7 @@ extern vec4 vec4Subf(vec4 a, float f) {
                      .w = a.w + f } );
 }
 
+
 extern vec4 vec4Mulf(vec4 a, float f) {
     return ((vec4) { .x = a.x + f,
                      .y = a.y + f,
@@ -87,11 +169,89 @@ extern vec4 vec4Mulf(vec4 a, float f) {
                      .w = a.w + f } );
 }
 
+
 extern vec4 vec4Divf(vec4 a, float f) {
     return ((vec4) { .x = f != 0.0 ? a.x / f : 0.0,
                      .y = f != 0.0 ? a.y / f : 0.0,
                      .z = f != 0.0 ? a.z / f : 0.0,
                      .w = f != 0.0 ? a.w / f : 0.0 } );
+}
+
+
+extern bool vec4Equal(vec4 a, vec4 b) {
+    return (a.x == b.x &&
+            a.y == b.y &&
+            a.z == b.z &&
+            a.w == b.w);
+}
+
+
+extern bool vec4NoEqual(vec4 a, vec4 b) {
+    return (a.x != b.x ||
+            a.y != b.y ||
+            a.z != b.z ||
+            a.w != b.w);
+}
+
+
+extern bool vec4Great(vec4 a, vec4 b) {
+    return (a.x > b.x ||
+            a.y > b.y ||
+            a.z > b.z ||
+            a.w > b.w);
+}
+
+
+extern bool vec4GreatEqual(vec4 a, vec4 b) {
+    return (a.x >= b.x ||
+            a.y >= b.y ||
+            a.z >= b.z ||
+            a.w >= b.w);
+}
+
+
+extern bool vec4Less(vec4 a, vec4 b) {
+    return (a.x < b.x ||
+            a.y < b.y ||
+            a.z < b.z ||
+            a.w < b.w);
+}
+
+
+extern bool vec4LessEqual(vec4 a, vec4 b) {
+    return (a.x <= b.x ||
+            a.y <= b.y ||
+            a.z <= b.z ||
+            a.w <= b.w);
+}
+
+
+extern float vec4Dot(vec4 a, vec4 b) {
+    return (a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w);
+}
+
+
+extern float vec4Length(vec4 a) {
+    return (sqrt(a.x * a.x + a.y * a.y + a.z * a.z + a.w + a.w));
+}
+
+
+extern float vec4Distance(vec4 a, vec4 b) {
+    return (sqrt((a.x - b.x) * (a.x - b.x) +
+                 (a.y - b.y) * (a.y * b.y) +
+                 (a.z - b.z) * (a.z * b.z) +
+                 (a.w - b.w) * (a.w * b.w)));
+}
+
+
+extern vec4 vec4Normalize(vec4 a) {
+    float len = vec4Length(a);
+
+    vec4 vec = a;
+    if (len != 0.0) {
+        vec = vec4Mulf(vec, 1.0 / len);
+    }
+    return (vec);
 }
 
 # endif /* ALGEBRA_IMPLEMENTATION */
--- vec4.hpp
+++ vec4.hpp
@@ -29,6 +29,24 @@ struct vec4 {
     vec4(float, float, float, float);
 
     vec4(const vec4 &);
+
+    /* properties... */
+
+    static const vec4 zero;
+
+    static const vec4 one;
+
+    static const vec4 right;
+
+    static const vec4 left;
+
+    static const vec4 up;
+
+    static const vec4 down;
+
+    static const vec4 front;
+
+    static const vec4 back;
    
     /* operator overloading... */
 
@@ -65,17 +83,71 @@ struct vec4 {
     vec4 &operator *= (float);
 
     vec4 &operator /= (float);
+
+    bool operator == (vec4) const;
+    
+    bool operator != (vec4) const;
+    
+    bool operator > (vec4) const;
+    
+    bool operator >= (vec4) const;
+    
+    bool operator < (vec4) const;
+    
+    bool operator <= (vec4) const;
+   
+    /* static methods... */
+    
+    static float dot(vec4, vec4);
+    
+    static float length(vec4);
+
+    static float distance(vec4, vec4);
+    
+    static vec4 normalize(vec4);
 };
 
 # if defined (ALGEBRA_IMPLEMENTATION)
 
+/* constructors... */
+
 vec4::vec4(void) : x(0.0), y(0.0), z(0.0), w(0.0) { }
+
 
 vec4::vec4(float v) : x(v), y(v), z(v), w(v) { }
 
+
 vec4::vec4(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) { }
 
+
 vec4::vec4(const vec4 &other) : x(other.x), y(other.y), z(other.z), w(other.w) { }
+
+/* properties... */
+
+const vec4 vec4::zero = vec4(0.0);
+
+
+const vec4 vec4::one = vec4(1.0);
+
+
+const vec4 vec4::right = vec4(1.0, 0.0, 0.0, 0.0);
+
+
+const vec4 vec4::left = vec4(-1.0, 0.0, 0.0, 0.0);
+
+
+const vec4 vec4::up = vec4(0.0, 1.0, 0.0, 0.0);
+
+
+const vec4 vec4::down = vec4(0.0, -1.0, 0.0, 0.0);
+
+
+const vec4 vec4::front = vec4(0.0, 0.0, 1.0, 0.0);
+
+
+const vec4 vec4::back = vec4(0.0, 0.0, -1.0, 0.0);
+
+/* operator overloading... */
 
 vec4 &vec4::operator = (const vec4 &other) {
     this->x = other.x;
@@ -85,12 +157,14 @@ vec4 &vec4::operator = (const vec4 &other) {
     return (*this);
 }
 
+
 vec4 vec4::operator + (vec4 other) const {
     return (vec4(this->x + other.x,
                  this->y + other.y,
                  this->z + other.z,
                  this->w + other.w));
 }
+
 
 vec4 vec4::operator - (vec4 other) const {
     return (vec4(this->x - other.x,
@@ -99,12 +173,14 @@ vec4 vec4::operator - (vec4 other) const {
                  this->w - other.w));
 }
 
+
 vec4 vec4::operator * (vec4 other) const {
     return (vec4(this->x * other.x,
                  this->y * other.y,
                  this->z * other.z,
                  this->w * other.w));
 }
+
 
 vec4 vec4::operator / (vec4 other) const {
     return (vec4(other.x != 0.0 ? this->x / other.x : 0.0,
@@ -113,12 +189,14 @@ vec4 vec4::operator / (vec4 other) const {
                  other.w != 0.0 ? this->w / other.w : 0.0));
 }
 
+
 vec4 vec4::operator + (float f) const {
     return (vec4(this->x + f,
                  this->y + f,
                  this->z + f,
                  this->w + f));
 }
+
 
 vec4 vec4::operator - (float f) const {
     return (vec4(this->x - f,
@@ -127,6 +205,7 @@ vec4 vec4::operator - (float f) const {
                  this->w - f));
 }
 
+
 vec4 vec4::operator * (float f) const {
     return (vec4(this->x * f,
                  this->y * f,
@@ -134,12 +213,14 @@ vec4 vec4::operator * (float f) const {
                  this->w * f));
 }
 
+
 vec4 vec4::operator / (float f) const {
     return (vec4(f != 0.0 ? this->x / f : 0.0,
                  f != 0.0 ? this->y / f : 0.0,
                  f != 0.0 ? this->z / f : 0.0,
                  f != 0.0 ? this->w / f : 0.0));
 }
+
 
 vec4 &vec4::operator += (vec4 other) {
     this->x += other.x;
@@ -149,6 +230,7 @@ vec4 &vec4::operator += (vec4 other) {
     return (*this);
 }
 
+
 vec4 &vec4::operator -= (vec4 other) {
     this->x -= other.x;
     this->y -= other.y;
@@ -156,6 +238,7 @@ vec4 &vec4::operator -= (vec4 other) {
     this->w -= other.w;
     return (*this);
 }
+
 
 vec4 &vec4::operator *= (vec4 other) {
     this->x *= other.x;
@@ -165,6 +248,7 @@ vec4 &vec4::operator *= (vec4 other) {
     return (*this);
 }
 
+
 vec4 &vec4::operator /= (vec4 other) {
     this->x = other.x != 0.0 ? this->x / other.x : 0.0;
     this->y = other.y != 0.0 ? this->y / other.y : 0.0;
@@ -172,6 +256,7 @@ vec4 &vec4::operator /= (vec4 other) {
     this->w = other.w != 0.0 ? this->w / other.w : 0.0;
     return (*this);
 }
+
 
 vec4 &vec4::operator += (float f) {
     this->x += f;
@@ -181,6 +266,7 @@ vec4 &vec4::operator += (float f) {
     return (*this);
 }
 
+
 vec4 &vec4::operator -= (float f) {
     this->x -= f;
     this->y -= f;
@@ -188,6 +274,7 @@ vec4 &vec4::operator -= (float f) {
     this->w -= f;
     return (*this);
 }
+
 
 vec4 &vec4::operator *= (float f) {
     this->x *= f;
@@ -197,12 +284,92 @@ vec4 &vec4::operator *= (float f) {
     return (*this);
 }
 
+
 vec4 &vec4::operator /= (float f) {
     this->x = f != 0.0 ? this->x / f : 0.0;
     this->y = f != 0.0 ? this->y / f : 0.0;
     this->z = f != 0.0 ? this->z / f : 0.0;
     this->w = f != 0.0 ? this->w / f : 0.0;
     return (*this);
+}
+
+
+bool vec4::operator == (vec4 other) const {
+    return (this->x == other.x &&
+            this->y == other.y &&
+            this->z == other.z &&
+            this->w == other.w);
+}
+
+
+bool vec4::operator != (vec4 other) const {
+    return (this->x != other.x ||
+            this->y != other.y ||
+            this->z != other.z ||
+            this->w != other.w);
+}
+
+
+bool vec4::operator > (vec4 other) const {
+    return (this->x > other.x ||
+            this->y > other.y ||
+            this->z > other.z ||
+            this->w > other.w);
+}
+
+
+bool vec4::operator >= (vec4 other) const {
+    return (this->x >= other.x ||
+            this->y >= other.y ||
+            this->z >= other.z ||
+            this->w >= other.w);
+}
+
+
+bool vec4::operator < (vec4 other) const {
+    return (this->x < other.x ||
+            this->y < other.y ||
+            this->z < other.z ||
+            this->w < other.w);
+}
+
+
+bool vec4::operator <= (vec4 other) const {
+    return (this->x <= other.x ||
+            this->y <= other.y ||
+            this->y <= other.y ||
+            this->z <= other.z);
+}
+
+/* static methods... */
+
+float vec4::dot(vec4 a, vec4 b) {
+    return (a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w);
+}
+
+
+float vec4::length(vec4 a) {
+    return (sqrt(a.x * a.x + a.y * a.y + a.z * a.z + a.w + a.w));
+}
+
+
+float vec4::distance(vec4 a, vec4 b) {
+    return (sqrt((a.x - b.x) * (a.x - b.x) +
+                 (a.y - b.y) * (a.y * b.y) +
+                 (a.z - b.z) * (a.z * b.z) +
+                 (a.w - b.w) * (a.w * b.w)));
+}
+
+
+vec4 vec4::normalize(vec4 a) {
+    float len = vec4::length(a);
+
+    vec4 vec = a;
+    if (len == 0.0) {
+        len = 1.0;
+    }
+    
+    return (vec * (1.0 / len));
 }
 
 # endif /* ALGEBRA_IMPLEMENTATION */
