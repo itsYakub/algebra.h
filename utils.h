4ab2ace057c1c8de6ed57fa47103e97a30f82458
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-05-14 10:14:31 +0200

    utils: updated module

--- utils.h
+++ utils.h
@@ -22,23 +22,48 @@
 # endif /* EPSILON */
 #
 # if !defined (deg2rad)
-#  define deg2rad(f) ((f) * ((PI) / 180.0))
+
+static inline float __algebra_deg2rad(float f) {
+    return (f * PI / 180.0);
+}
+
+#  define deg2rad __algebra_deg2rad
 # endif /* deg2rad */
 #
 # if !defined (rad2deg)
-#  define rad2deg(f) ((f) * (180.0 / (PI)))
+
+static inline float __algebra_rad2deg(float f) {
+    return (f * 180.0 / PI);
+}
+
+#  define rad2deg __algebra_rad2deg
 # endif /* rad2deg */
 #
 # if !defined (min)
-#  define min(a, b) ((a) < (b) ? (a) : (b))
+
+static inline float __algebra_min(float a, float b) {
+    return (a < b ? a : b);
+}
+
+#  define min __algebra_min
 # endif /* min */
 #
 # if !defined (max)
-#  define max(a, b) ((a) > (b) ? (a) : (b))
+
+static inline float __algebra_max(float a, float b) {
+    return (a > b ? a : b);
+}
+
+#  define max __algebra_max
 # endif /* max */
 #
 # if !defined (clamp)
-#  define clamp(f, a, b) (min(max((f), (a)), (b)))
+
+static inline float __algebra_clamp(float f, float a, float b) {
+    return (min(max(f, a), b));
+}
+
+#  define clamp __algebra_clamp
 # endif /* clamp */
 #
 # if !defined (clamp01)
@@ -46,11 +71,45 @@
 # endif /* clamp01 */
 #
 # if !defined (lerp)
-#  define lerp(a, b, t) (((b) - (a)) * (t) + (a))
+
+static inline float __algebra_lerp(float a, float b, float t) {
+    return (b - a * t + a);
+}
+
+#  define lerp __algebra_lerp
 # endif /* lerp */
 #
 # if !defined (lerp01)
-#  define lerp01(a, b, t) (lerp(a, b, clamp01(t)))
+#  define lerp01(a, b, t) (lerp((a), (b), clamp01((t))))
 # endif /* lerp01 */
+#
+# if !defined (saturate)
+
+static inline float __algebra_saturate(float a) { 
+    return (max(0.0, min(1.0, a)));
+}
+
+#  define saturate __algebra_saturate
+# endif /* saturate */
+#
+# if !defined (step)
+
+static inline float __algebra_step(float a, float x) {
+    return (x > a ? 1.0 : 0.0);
+}
+
+#  define step __algebra_step
+# endif /* step */
+#
+# if !defined (smoothstep)
+
+static inline float __algebra_smoothstep(float e0, float e1, float x) {
+    float t = saturate((x - e0) / (e1 - e0));
+
+    return (t * t * (3.0 - (2.0 * t)));
+}
+
+#  define smoothstep __algebra_smoothstep
+# endif /* smoothstep */
 #
 #endif /* _utils_h_ */
