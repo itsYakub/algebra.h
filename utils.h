a535d8b7d4d99768b47338e64c5adc02010097fe
Jakub Oleksiak <yakubofficialmail@gmail.com>
2026-05-25 09:57:00 +0200

    utils: updated utils module

--- utils.h
+++ utils.h
@@ -21,104 +21,82 @@
 #  define INFINITY (1e100)
 # endif /* EPSILON */
 #
-# if !defined (deg2rad)
+# if !defined ALGAPI
+#  define ALGAPI extern inline
+# endif /* ALGAPI */
 
-static inline float __algebra_deg2rad(float f) {
+ALGAPI float alg_deg2rad(float);
+
+ALGAPI float alg_rad2deg(float);
+
+ALGAPI float alg_min(float, float);
+
+ALGAPI float alg_max(float, float);
+
+ALGAPI float alg_fract(float);
+
+ALGAPI float alg_clamp(float, float, float);
+
+ALGAPI float alg_lerp(float, float, float);
+
+ALGAPI float alg_saturate(float); 
+
+ALGAPI float alg_step(float, float);
+
+ALGAPI float alg_smoothstep(float, float, float);
+
+# if defined (ALGEBRA_IMPLEMENTATION)
+
+ALGAPI float alg_deg2rad(float f) {
     return (f * PI / 180.0);
 }
 
-#  define deg2rad __algebra_deg2rad
-# endif /* deg2rad */
-#
-# if !defined (rad2deg)
 
-static inline float __algebra_rad2deg(float f) {
+ALGAPI float alg_rad2deg(float f) {
     return (f * 180.0 / PI);
 }
 
-#  define rad2deg __algebra_rad2deg
-# endif /* rad2deg */
-#
-# if !defined (min)
 
-static inline float __algebra_min(float a, float b) {
+ALGAPI float alg_min(float a, float b) {
     return (a < b ? a : b);
 }
 
-#  define min __algebra_min
-# endif /* min */
-#
-# if !defined (max)
 
-static inline float __algebra_max(float a, float b) {
+ALGAPI float alg_max(float a, float b) {
     return (a > b ? a : b);
 }
 
-#  define max __algebra_max
-# endif /* max */
-#
-# if !defined (fract)
 
-static inline float __algebra_fract(float a) {
+ALGAPI float alg_fract(float a) {
     return (a - floorf(a));
 }
 
-#  define fract __algebra_fract
-# endif /* fract */
-#
-# if !defined (clamp)
 
-static inline float __algebra_clamp(float f, float a, float b) {
+ALGAPI float alg_clamp(float f, float a, float b) {
     return (min(max(f, a), b));
 }
 
-#  define clamp __algebra_clamp
-# endif /* clamp */
-#
-# if !defined (clamp01)
-#  define clamp01(f) (clamp((f), 0.0, 1.0))
-# endif /* clamp01 */
-#
-# if !defined (lerp)
 
-static inline float __algebra_lerp(float a, float b, float t) {
+ALGAPI float alg_lerp(float a, float b, float t) {
     return (b - a * t + a);
 }
 
-#  define lerp __algebra_lerp
-# endif /* lerp */
-#
-# if !defined (lerp01)
-#  define lerp01(a, b, t) (lerp((a), (b), clamp01((t))))
-# endif /* lerp01 */
-#
-# if !defined (saturate)
 
-static inline float __algebra_saturate(float a) { 
+ALGAPI float alg_saturate(float a) { 
     return (max(0.0, min(1.0, a)));
 }
 
-#  define saturate __algebra_saturate
-# endif /* saturate */
-#
-# if !defined (step)
 
-static inline float __algebra_step(float a, float x) {
+ALGAPI float alg_step(float a, float x) {
     return (x > a ? 1.0 : 0.0);
 }
 
-#  define step __algebra_step
-# endif /* step */
-#
-# if !defined (smoothstep)
 
-static inline float __algebra_smoothstep(float e0, float e1, float x) {
+ALGAPI float alg_smoothstep(float e0, float e1, float x) {
     float t = saturate((x - e0) / (e1 - e0));
 
     return (t * t * (3.0 - (2.0 * t)));
 }
 
-#  define smoothstep __algebra_smoothstep
-# endif /* smoothstep */
-#
+# endif /* ALGEBRA_IMPLEMENTATION */
 #endif /* _utils_h_ */
